92e5db8b54c14bb1f78603624801ef50c706301c
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-17 06:36:36 +0530

    done

--- tests/test_book.py
+++ tests/test_book.py
@@ -2,6 +2,7 @@ from src.book import Book
 from src.staff import Staff
 
 import pytest
+from unittest.mock import Mock
 
 
 @pytest.fixture
@@ -11,9 +12,16 @@ def book():
 
 @pytest.fixture
 def staff():
-    return Staff('Test Staff', 'Test City')
+    staff_stub = Mock(Staff)
+    staff_stub.name.return_value = 'Stub Staff'
+    return staff_stub
 
 
 def test_register(book, staff):
     book.register(staff)
     assert book.registered_by == staff
+
+
+def test_registration_has_the_staff_name(book, staff):
+    book.register(staff)
+    assert book.registered_by.name == staff.name
