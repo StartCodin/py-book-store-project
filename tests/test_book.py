bb8b41fc7f462b844c3be6a9d269fd13a69e0db0
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-17 06:43:58 +0530

    done

--- tests/test_book.py
+++ tests/test_book.py
@@ -1,5 +1,6 @@
 from src.book import Book
 from src.staff import Staff
+from src.constants import *
 
 import pytest
 from unittest.mock import Mock
@@ -10,18 +11,7 @@ def book():
     return Book('Test Book', 'Textbook', 900, 2, 'Test Author', 'Test Publisher', 1000)
 
 
-@pytest.fixture
-def staff():
-    staff_stub = Mock(Staff)
-    staff_stub.name.return_value = 'Stub Staff'
-    return staff_stub
-
-
-def test_register(book, staff):
-    book.register(staff)
-    assert book.registered_by == staff
-
-
-def test_registration_has_the_staff_name(book, staff):
-    book.register(staff)
-    assert book.registered_by.name == staff.name
+@pytest.mark.parametrize('book, category', [(book, cat) for cat in CATEGORY])
+def test_category_in_the_list_is_accepted(book, category):
+    book.category = category
+    assert book.category == category
