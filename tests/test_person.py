1f1b72fb41dfeda299a0857788a6cb98a63500ca
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-17 13:28:58 +0530

    done

--- tests/test_person.py
+++ tests/test_person.py
@@ -12,9 +12,10 @@ def test_person_created_with_valid_arguments(person):
     assert person.name == 'Test Name' and person.city == 'Test City'
 
 
-def test_person_with_name_below_min_length_raises_value_error(person):
+@pytest.mark.parametrize('name', [(n) for n in ('', 'A', 'AB', 'ABC', 'ABCD')])
+def test_person_with_name_below_min_length_raises_value_error(person, name):
     with pytest.raises(ValueError):
-        person.name = ''
+        person.name = name
 
 
 def test_person_with_name_over_max_length_raises_value_error(person):
